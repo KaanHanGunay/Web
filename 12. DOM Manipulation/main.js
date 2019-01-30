2230a98fa5ed5954d81f5481c0d90424f5a0672a
KaanHGunay <kaangunay16@gmail.com>
2019-01-30 23:06:21 +0300

    JS exercise

--- 12. DOM Manipulation/main.js
+++ 12. DOM Manipulation/main.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
 
 function inputLenght() {
     return input.value.length;
@@ -8,9 +9,10 @@ function inputLenght() {
 
 function createListElement() {
     var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.append(li);
-        input.value = "";
+    li.appendChild(document.createTextNode(input.value));
+    ul.append(li);
+    input.value = "";
+    addDeleteButton(li);
 }
 
 function addListAfterClick() {
@@ -23,6 +25,26 @@ function addListAfterKeyPress(event) {
     if (inputLenght() > 0 && event.keyCode === 13) {
         createListElement();
     } 
+}
+
+function addDeleteButton(parent) {
+    var deleteButton = document.createElement("button");
+    deleteButton.appendChild(document.createTextNode("Sil"));
+    deleteButton.addEventListener("click", function () {
+        ul.removeChild(parent);
+    })
+    parent.appendChild(deleteButton);
+}
+
+function addChangeClassEvent (element) {
+    element.addEventListener("click", function () {
+        element.classList.toggle('done');
+    });
+}
+
+for (let i = 0; i < li.length; i++) {
+    addDeleteButton(li[i]);
+    addChangeClassEvent(li[i]);
 }
 
 button.addEventListener("click", addListAfterClick);
