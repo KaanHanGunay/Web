c591629ca9a1e298808906c7978ed6ecbf4ed8d3
KaanHGunay <kaangunay16@gmail.com>
2019-03-02 22:26:34 +0300

    server

--- final_project/src/App.js
+++ final_project/src/App.js
@@ -34,8 +34,27 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedin: false
+      isSignedin: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({ 
+        user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+      }
+    })
   }
 
   calculateFaceLocation = (data) => {
@@ -92,7 +111,7 @@ class App extends Component {
           </div> : (
             route === 'signin' 
             ? <Singin onRouteChange={this.onRouteChange} />
-            : <Register onRouteChange={this.onRouteChange} />
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )          
         }
       </div>
